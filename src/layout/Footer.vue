c39ec81e226b200ef70ceb4f05f4c48d2fdb1a0a
mengweijin <1002284406@qq.com>
2022-05-04 22:14:06 +0800

    update

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,3 +1,7 @@
+<script setup>
+  const currentYear=new Date().getFullYear()
+</script>
+
 <template>
   <div>
       <span style="color: #f313b9; font-size: 20px; width: 100px; margin-right: 20px;">QuickBoot UI</span>
@@ -5,9 +9,8 @@
       <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1002284406&site=qq&menu=yes" style="margin-right: 20px;">
           QQ
       </a>
-      mail: 
-      <a href="mailto:mengweijin.work@foxmail.com" style="margin-right: 20px;">mengweijin.work@foxmail.com </a>
-      <span>The Apache License Copyright © 2021</span>
+      The Apache License 2.0 Copyright © 2018~<span>{{currentYear}}</span> Meng Wei Jin
+		  &lt;<a href="mailto:mengweijin.work@foxmail.com">mengweijin.work@foxmail.com</a>&gt;
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueRouter from './router/router.js'
 import Http from './utils/http.js'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(VueRouter)
 app.mount('#app')
 app.config.globalProperties.$http = Http
