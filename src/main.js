d231c039c6e8788316d79e0127cee36d6825e347
mengweijin <1002284406@qq.com>
2022-05-04 20:12:16 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueRouter from './router/router.js'
+import Http from './utils/http.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueRouter)
+app.mount('#app')
+app.config.globalProperties.$http = Http
---
+++ src/router/router.js
@@ -0,0 +1,60 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Layout from '../components/layout/Layout.vue'
+import Header from '../components/Header.vue'
+
+const routes = [
+    { 
+        path: '/', 
+        component: Layout,
+        redirect: '/goods',
+        children: [
+            {
+                path: '/goods',
+                components: {
+                    Header: Header,
+                    default: () => import ('../components/layout/LayoutGoods.vue')
+                }
+            },
+            {
+                path: '/goods/:goodsTypeId',
+                components: {
+                    Header: Header,
+                    default: () => import ('../components/layout/LayoutGoods.vue')
+                }
+            },
+            {
+                path: '/goodsType',
+                components: {
+                    Header: Header,
+                    default: () => import ('../components/layout/LayoutGoodsType.vue')
+                }
+            },
+            {
+                path: '/stock-in',
+                components: {
+                 Header: Header,
+                 default: () => import ('../components/StockIn.vue')
+                }
+            },
+            {
+                path: '/stock-out',
+                components: {
+                 Header: Header,
+                 default: () => import ('../components/StockOut.vue')
+                }
+            }
+        ]
+    }
+  ]
+
+// 路由根路径：'/relx/'; 为什么设置路由根路径？避免路由路径和后台接口路径一样，当刷新页面时变成调用后台接口而返回 JSON 的问题。
+// 部署到 nginx 服务器下，我们一般 router 的 hitory 使用 createWebHistory 来去掉 url 中的 #
+// 但如果要打包并部署到 springboot 下的 static 目录，默认只能用 createWebHashHistory 才不会有问题。
+// 但我们又想使用 createWebHistory 模式，还不想刷新时出现页面 404 的问题，此时需要在后端处理了。
+// 参考 java 类：NotFoundErrorPageRegistrar.java
+const router = createRouter({
+    history: createWebHistory('/relx/'),
+    routes, 
+})
+
+export default router
